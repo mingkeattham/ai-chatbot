85f8ef1cbe83041a382f5fc88d25191220257a24
Ming Keat <mingkeat.tham@secur.sg>
2025-03-09 15:24:06 +0800

    Added gemini support

--- lib/ai/providers.ts
+++ lib/ai/providers.ts
@@ -11,6 +11,7 @@ import {
   reasoningModel,
   titleModel,
 } from './models.test';
+import { createGoogleGenerativeAI } from '@ai-sdk/google';
 
 export const myProvider = isTestEnvironment
   ? customProvider({
@@ -20,6 +21,7 @@ export const myProvider = isTestEnvironment
         'chat-model-reasoning': reasoningModel,
         'title-model': titleModel,
         'artifact-model': artifactModel,
+        'gemini' : createGoogleGenerativeAI({apiKey : process.env.GEMINI_API_KEY}).languageModel('gemini-2.0-flash-001'),
       },
     })
   : customProvider({
@@ -28,6 +30,8 @@ export const myProvider = isTestEnvironment
         'chat-model-large': openai('gpt-4o'),
         'title-model': openai('gpt-4-turbo'),
         'artifact-model': openai('gpt-4o-mini'),
+        'gemini' : createGoogleGenerativeAI({apiKey : process.env.GEMINI_API_KEY}).languageModel('gemini-2.0-flash-001'),
+
       },
       imageModels: {
         'small-model': openai.image('dall-e-2'),
